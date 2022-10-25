5ed386289d6bca6cf3495c664dd65153d657a7ea
extrastufff054 <108084022+extrastufff054@users.noreply.github.com>
2022-10-25 21:25:34 +0530

    Update Javascript_vs_core_Java.java

--- Assingments./Javascript_vs_core_Java.java
+++ Assingments./Javascript_vs_core_Java.java
@@ -1,1 +1,10 @@
-Java                          JavaScript
+"Java"                                        "JavaScript"
+1)Java is considered a object-oriented      1)JavaScript is a light weight programming language(Scripting language)
+and has a vietual machine that allow us to    and is used to make web pages interactive.
+create compiled progeams that run on nearly   
+every platform .
+                                            2)It can insert dynamic text into HTML.
+                                            3)JS is not similar or related to JAVA.
+                                            4)It has a similar syntax to C language and is widely used in client-side 
+                                              and server-side Web applications, but there are few similarities only.
+                                              
